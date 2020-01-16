14dd8836bc5ec79c254c69718ad126c8eb46b876
fumikomimura <lomilomikapolei@gmail.com>
2020-01-16 16:44:05 +0900

    Initial Commit

---
+++ Hello world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by 三村文子 on 2020/01/16.
+//  Copyright © 2020 三村文子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
